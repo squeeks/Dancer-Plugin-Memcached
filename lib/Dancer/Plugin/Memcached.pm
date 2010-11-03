ca4ac9017562d0b60ed7fe7e4e4d760dcf2f8374
Squeeks <privacymyass@gmail.com>
2010-11-03 11:05:48 +0000

    Adding bug-reporting boilerplate since dzil isn't.

--- lib/Dancer/Plugin/Memcached.pm
+++ lib/Dancer/Plugin/Memcached.pm
@@ -137,6 +137,42 @@ register_plugin;
 
 Squeeks, C<< <squeek at cpan.org> >>
 
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-dancer-plugin-memcached at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dancer-Plugin-Memcached>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Dancer::Plugin::Memcached
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dancer-Plugin-Memcached>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Dancer-Plugin-Memcached>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Dancer-Plugin-Memcached>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Dancer-Plugin-Memcached/>
+
+=back
+
+
 =head1 SEE ALSO
 
 Dancer Web Framework - L<Dancer>
